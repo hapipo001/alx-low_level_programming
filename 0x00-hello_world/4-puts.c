87285fa5eff39e9d087f2fbc29159dce02079331
hapipo001 <rhapipo@gmail.com>
2023-02-16 13:48:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-include <stdio.h>
+#include <stdio.h>
 /**
- * main - A program that prints a line with puts functions
+ * main - A program that prints a line with puts function
  * Return: 0 (Success)
  */
 int main(void)
