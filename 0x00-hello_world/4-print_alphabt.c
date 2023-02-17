78a9e25d5c6932d411660337435de453cecf7469
hapipo001 <rhapipo@gmail.com>
2023-02-17 14:37:04 -0800

    4-print_alphabt.c

---
+++ 0x00-hello_world/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+putchar(ch);
+}
+ch++;
+}
+putcha('\n');
+return (0);
+}
