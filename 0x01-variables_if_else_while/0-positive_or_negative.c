a6f58427fe8dae7f549ea21bf250aa4c2bc8627e
hapipo001 <rhapipo@gmail.com>
2023-02-17 12:14:40 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,9 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @void: Null value
+ * Description: Check the last digit of the number
+ * Return: Zero value to succeful
+ */
+int main(void)
