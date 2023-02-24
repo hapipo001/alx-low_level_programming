7a8578f7baf86f90c69dd40b876eed3635b69f75
hapipo001 <rhapipo@gmail.com>
2023-02-24 12:44:45 -0800

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -10,4 +9,3 @@ int convert_day(int month, int day);
 void print_times_table(int n);
 
 #endif
-
